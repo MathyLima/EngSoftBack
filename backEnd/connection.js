0eab05b4c2170b0f0fd3771548caaeff484f4a38
MathyLima <mathy.l.freitas@gmail.com>
2024-05-07 19:58:47 -0300

    Back end da aplicação de engenharia de software, feita em Node

--- backEnd/connection.js
+++ backEnd/connection.js
@@ -1,64 +1,200 @@
 const mysql = require('mysql2');
 
-class Connection{
-    constructor(host,user,password,database){
-        this.host=host;
-        this.user = user;
-        this.password = password;
-        this.database = database;
-        
+class Connection {
+  constructor(host, user, password, database) {
+    this.host = host;
+    this.user = user;
+    this.password = password;
+    this.database = database;
 
-        this.connection = mysql.createConnection({
-            host:this.host,
-            user:this.user,
-            database:this.database,
-            password:this.password
-        })
+    this.connection = mysql.createConnection({
+      host: this.host,
+      user: this.user,
+      database: this.database,
+      password: this.password
+    });
 
-        this.connect();
-    }
+    this.connect();
+  }
 
-    connect(){
-        this.connection.connect((err)=>{
-            if (err){
-                console.error('Erro ao conectar ao banco de dados',err);
-                return
-            }
-            console.log('Conectado ao banco de dados com sucesso!');
-        });
-    }
+  connect() {
+    this.connection.connect((err) => {
+      if (err) {
+        console.error('Erro ao conectar ao banco de dados', err);
+        return;
+      }
+      console.log('Conectado ao banco de dados com sucesso!');
+    });
+  }
 
-    getTableColumns(tableName,callback){
-        const sql = `DESCRIBE ${tableName}`;
-        this.connection.query(sql,(err,results)=>{
-            if(err){
-                console.error('Erro ao obter os nomes das colunas: ',err)
-            }else{
-                const columnNames = results.map((row) => row.Field);
-                const filtered_columns = columnNames.filter(columnName=>columnName !== 'id');
-                const columns = filtered_columns.join(', ')
-                callback(null,columns);
-            }
-        })
-    }
+  getTableColumns(tableName, callback) {
+    const sql = `DESCRIBE ${tableName}`;
+    this.connection.query(sql, (err, results) => {
+      if (err) {
+        console.error('Erro ao obter os nomes das colunas: ', err);
+        callback(err);
+      } else {
+        const id = tableName === "vendedor" ? "id_vended" : "id_" + tableName;
 
-//create
-createRow(tableName, values, callback) {
+        if (Array.isArray(results)) {
+          const columnNames = results.map((row) => row.Field);
+          const filtered_columns = columnNames.filter(columnName => columnName !== id);
+          const columns = filtered_columns.join(', ');
+          callback(null, columns);
+        } else {
+          callback(new Error('A consulta não retornou resultados válidos.'));
+        }
+      }
+    });
+  }
+
+  createRow(tableName, values, callback) {
     this.getTableColumns(tableName, (err, columnNames) => {
       if (err) {
         console.error('Erro ao obter os nomes das colunas:', err);
         callback(err);
       } else {
-        const placeholders = columnNames.split(',').map(() => '?').join(', ');
+        // Mapeie os valores para adicionar aspas apenas quando necessário
+        const placeholders = columnNames
+          .split(',')
+          .map((colName, index) => (colName.trim() === 'is_lead' ? '?' : '?'))
+          .join(', ');
+  
         const sql = `INSERT INTO ${tableName} (${columnNames}) VALUES (${placeholders})`;
   
-        // Execute a consulta SQL para inserir os valores
         this.connection.query(sql, values, (err, results) => {
           if (err) {
             console.error('Erro ao inserir registro', err);
             callback(err);
           } else {
-            console.log('Registro inserido com sucesso!');
+            const insertedId = results.insertId; // Obtém o ID do registro inserido
+            console.log('Registro inserido com sucesso! ID:', insertedId);
+            callback(null, insertedId); // Retorna o ID como parte do callback
+          }
+        });
+      }
+    });
+  }
+
+  searchByIdClient(tableName,clienteId, callback) {
+    const sql = `SELECT * FROM ${tableName}  WHERE id_cliente = ?`;
+    this.connection.query(sql, [clienteId], (err, results) => {
+      if (err) {
+        console.error("Erro ao buscar interesses", err);
+        callback(err);
+      } else {
+        if (results.length > 0) {
+          console.log("Interesses encontrados!");
+        } else {
+          console.log("Nenhum interesse encontrado para o cliente.");
+        }
+        callback(null, results);
+      }
+    });
+  }
+
+  getEtapaAndClient(clientId, callback) {
+    const sql = "SELECT DISTINCT cliente.*, abordagem.etapa FROM cliente JOIN abordagem ON abordagem.id_cliente = ?";
+    
+    this.connection.query(sql, [clientId], (err, results) => {
+      if (err) {
+        console.error("Erro ao buscar interesses", err);
+        callback(err);
+      } else {
+        if (results.length > 0) {
+          console.log("Interesses encontrados!");
+        } else {
+          console.log("Nenhum interesse encontrado para o cliente.");
+        }
+        callback(null, results);
+      }
+    });
+  }
+  
+  
+  search_all(tableName, callback) {
+    const sql = `SELECT * FROM ${tableName}`;
+    this.connection.query(sql, (err, results) => {
+      if (err) {
+        console.error('Erro ao listar clientes', err);
+        callback(err);
+      } else {
+        if (results.length > 0) {
+          console.log('Clientes listados!');
+        } else {
+          console.log('Nenhum registro encontrado!');
+        }
+        callback(null, results);
+      }
+    });
+  }
+
+
+
+  research_by_id(tableName, id, callback) {
+    const id_table = tableName === "vendedor" ? "id_vended" : "id_" + tableName;
+
+
+    const sql = `SELECT * FROM ${tableName} WHERE ${id_table} = '${id}'`;
+    this.connection.query(sql, (err, results) => {
+      if (err) {
+        console.error('Erro ao pesquisar nome', err);
+        callback(err);
+      } else {
+        if (results.length > 0) {
+          console.log('Registros encontrados!');
+        } else {
+          console.log('Nenhum registro encontrado');
+        }
+        callback(null, results);
+      }
+    });
+  }
+
+
+  research_by_cpf(tableName,cpf, callback) {
+    
+
+  
+    const sql = `SELECT nome_vended FROM ${tableName} WHERE cpf = '${cpf}'`;
+    this.connection.query(sql, (err, results) => {
+      if (err) {
+        console.error('Erro ao pesquisar nome', err);
+        callback(err);
+      } else {
+        if (results.length > 0) {
+          console.log('Registros encontrados!');
+        } else {
+          console.log('Nenhum registro encontrado');
+        }
+        callback(null, results);
+      }
+    });
+  }
+
+  updateRow(tableName, id, values, callback) {
+    const id_column = tableName === "vendedor" ? "id_vended" : "id_" + tableName;
+  
+    this.getTableColumns(tableName, (err, columnNames) => {
+      if (err) {
+        console.error('Erro ao obter os nomes das colunas:', err);
+        callback(err);
+      } else {
+        const setClause = columnNames
+          .split(', ')
+          .map((colName, index) => (colName.trim() === 'is_lead' ? 'is_lead = ?' : colName.trim() + ' = ?'))
+          .join(', ');
+  
+        const sql = `UPDATE ${tableName} SET ${setClause} WHERE ${id_column} = ?`;
+  
+        values.push(id);
+  
+        this.connection.query(sql, values, (err, results) => {
+          if (err) {
+            console.error('Erro ao atualizar registro', err);
+            callback(err);
+          } else {
+            console.log('Registro atualizado com sucesso!');
             callback(null, results);
           }
         });
@@ -66,96 +202,49 @@ createRow(tableName, values, callback) {
     });
   }
   
-  
 
-    search_all(tableName,callback){
-        const sql = `SELECT * FROM ${tableName}`;
-        this.connection.query(sql,(err,results)=>{
-            if(err){
-                console.error('Erro ao listar clientes');
-                callback(err)
-            }else{
-                if(results.length > 0){
-                console.log('Clientes listados!');
-                }else{
-                    console.log('Nenhum registro encontrado!');
-                }
-                callback(null,results);
-            }
-        });
-    }
-    //research
-    research_by_name(tableName,name,callback){
-        const sql = `SELECT * FROM ${tableName} WHERE nome_cliente = '${name}' `;
-        this.connection.query(sql,(err,results)=>{
-            if(err){
-                console.error('Erro ao pesquisar nome');
-                callback(err);
-            }else{
-                if (results.length > 0){
-                    console.log('Registros encontrados!');
-                }
-                    else{
-                        console.log('Nenhum registro encontrado');
-                    }
-                callback(null,results);
-                
-            }
-        });
-    }
-    updateRow(tableName, id, values, callback) {
-        this.getTableColumns(tableName, (err, columnNames) => {
-          if (err) {
-            console.error('Erro ao obter os nomes das colunas:', err);
-            callback(err);
-          } else {
-            // Construir a cláusula SET com base nas colunas e valores fornecidos
-            const setClause = columnNames
-              .map((columnName) => `${columnName} = ?`)
-              .join(', ');
-      
-            const sql = `UPDATE ${tableName} SET ${setClause} WHERE id = ?`;
-      
-          // Adicione o ID ao final do array de valores
-            values.push(id);
-      
-            // Execute a consulta SQL para atualização
-            this.connection.query(sql, values, (err, results) => {
-              if (err) {
-                console.error('Erro ao atualizar registro', err);
-                callback(err);
-              } else {
-                console.log('Registro atualizado com sucesso!');
-                callback(null, results);
-              }
-            });
-          }
-        });
+  deleteRow(tableName, id, callback) {
+    const id_tabela = tableName === "vendedor" ? "id_vended" : "id_" + tableName;
+
+    const sql = `DELETE FROM ${tableName} WHERE ${id_tabela} = ?`;
+
+    this.connection.query(sql, [id], (err, results) => {
+      if (err) {
+        console.error('Erro ao remover registro', err);
+        callback(err);
+      } else {
+        console.log('Registro removido com sucesso!');
+        callback(null, results);
       }
-    //deletar
-    deleteRow(tableName, id, callback) {
-        const sql = `DELETE FROM ${tableName} WHERE id = '${id}'`;
-      
-        this.connection.query(sql, (err, results) => {
-          if (err) {
-            console.error('Erro ao remover registro', err);
-            callback(err);
-          } else {
-            console.log('Registro removido com sucesso!');
-            callback(null, results);
-          }
-        });
-      }
+    });
+  }
+
+  login(cpf, senha, callback) {
+    const tableName = 'vendedor';
+    const sql = `SELECT * FROM ${tableName} WHERE cpf = '${cpf}' AND senha = '${senha}'`;
+
+    this.connection.query(sql, (err, results) => {
+      if (err) {
+        console.error("Esse login não existe", err);
+        callback(err);
+      } else {
+        if (results.length > 0) {
+          console.log("Registros encontrados!");
+        } else {
+          console.log("Nenhum registro encontrado");
+        }
+        callback(null, results);
+    }
+});
+}
 }
 
-//preencha com seu login e senha do SGBD
+// Preencha com seu login e senha do SGBD
 const loginBD = {
-  user:'root',
-  password:'Youngmull4!'
+  user: 'root',
+  password: 'Youngmull4!'
 }
 
+const connection = new Connection('localhost', loginBD.user, loginBD.password, 'crm_es');
 
-
-const connection = new Connection('localhost',loginBD.user,loginBD.password,'crm_es');
-
-module.exports = connection
+module.exports = connection;
--- backEnd/routes/routes.js
+++ backEnd/routes/routes.js
@@ -7,25 +7,31 @@ const router = express.Router();
 router.put('/api/update_client/:id', (req, res) => {
   const { id } = req.params;
   const {
+    cpf,
     nome,
-    busto_largura,
-    busto_altura,
-    cintura_largura,
-    cintura_altura,
-    quadril_largura,
-    quadril_altura,
+    email,
+    telefone_1,
+    telefone_2,
+    idade,
+    endereco,
+    cep,
+    atual_vendedor,
+    is_lead
   } = req.body;
   const values = [
+    cpf,
     nome,
-    busto_largura,
-    busto_altura,
-    cintura_largura,
-    cintura_altura,
-    quadril_largura,
-    quadril_altura,
+    email,
+    telefone_1,
+    telefone_2,
+    idade,
+    endereco,
+    cep,
+    atual_vendedor,
+    is_lead
   ];
 
-  connection.updateRow('clientes', id, values, (err, result) => {
+  connection.updateRow('cliente', id, values, (err, result) => {
     if (err) {
       console.error('Erro ao atualizar um cliente:', err);
       res.status(500).json({ error: 'Erro ao atualizar um cliente' });
@@ -36,33 +42,163 @@ router.put('/api/update_client/:id', (req, res) => {
   });
 });
 
+router.put('/api/update_vendedor/:id', (req, res) => {
+  const { id } = req.params;
+  const {
+    cpf,
+    senha,
+    nome_vended,
+    email_vended,
+    tell_1,
+    tell_2,
+    endereco,
+    cep,
+  } = req.body;
+  const values = [
+    cpf,
+    senha,
+    nome_vended,
+    email_vended,
+    tell_1,
+    tell_2,
+    endereco,
+    cep,
+  ];
+
+  connection.updateRow('vendedor', id, values, (err, result) => {
+    if (err) {
+      console.error('Erro ao atualizar um vendedor:', err);
+      res.status(500).json({ error: 'Erro ao atualizar um vendedor' });
+    } else {
+      console.log('Vendedor atualizado com sucesso!');
+      res.status(200).json({ message: 'Vendedor atualizado com sucesso!' });
+    }
+  });
+});
+
 router.post('/api/create_client', (req, res) => {
   const {
+    cpf,
     nome,
-    busto_largura,
-    busto_altura,
-    cintura_largura,
-    cintura_altura,
-    quadril_largura,
-    quadril_altura,
+    email,
+    telefone_1,
+    telefone_2,
+    idade,
+    endereco,
+    cep,
+    atual_vendedor,
+    is_lead
   } = req.body;
 
   const values = [
+    cpf,
     nome,
-    busto_largura,
-    busto_altura,
-    cintura_largura,
-    cintura_altura,
-    quadril_largura,
-    quadril_altura,
+    email,
+    telefone_1,
+    telefone_2,
+    idade,
+    endereco,
+    cep,
+    atual_vendedor,
+    is_lead,
   ];
-  connection.createRow('clientes', values, (err, result) => {
+
+  connection.createRow('cliente', values, (err, result) => {
     if (err) {
       console.error('Erro ao criar um cliente:', err);
       res.status(500).json({ error: 'Erro ao criar um cliente' });
     } else {
-      console.log('Cliente criado com sucesso!');
-      res.status(200).json({ message: 'Cliente criado com sucesso!' });
+      const clienteID = result // Obtém o ID do cliente inserido
+      console.log('Cliente criado com sucesso! ID:', clienteID);
+      res.status(200).json({ message: 'Cliente criado com sucesso!', id_cliente: clienteID });
+    }
+  });
+});
+
+router.post('/api/create_vendedor', (req, res) => {
+  const {
+    cpf,
+    senha,
+    nome,
+    email,
+    telefone_1,
+    telefone_2,
+    endereco,
+    cep,
+  } = req.body;
+
+const values = [
+  cpf,
+  senha,
+  nome,
+  email,
+  telefone_1,
+  telefone_2,
+  endereco,
+  cep,
+];
+
+  connection.createRow('vendedor', values, (err, result) => {
+    if (err) {
+      console.error('Erro ao criar um vendedor:', err);
+      res.status(500).json({ error: 'Erro ao criar um vendedor' });
+    } else {
+      console.log('Vendedor criado com sucesso!');
+      res.status(200).json({ message: 'Vendedor criado com sucesso!' });
+    }
+  });
+});
+router.post('/api/create_abordagem', (req, res) => {
+  const {
+    etapa,
+    data_inicio,
+    data_fim,
+    id_cliente,
+    id_vended
+  } = req.body;
+
+const values = [
+  etapa,
+  data_inicio,
+  data_fim,
+  id_cliente,
+  id_vended
+];
+
+  connection.createRow('abordagem', values, (err, result) => {
+    if (err) {
+      console.error('Erro ao criar um vendedor:', err);
+      res.status(500).json({ error: 'Erro ao criar um vendedor' });
+    } else {
+      console.log('Vendedor criado com sucesso!');
+      res.status(200).json({ message: 'Vendedor criado com sucesso!' });
+    }
+  });
+});
+
+router.post('/api/create_interesse', (req, res) => {
+  const {
+    modelo,
+    limite_preco,
+    cor,
+    categoria,
+    id_cliente,
+  } = req.body;
+
+  const values = [
+    modelo,
+    limite_preco,
+    cor,
+    categoria,
+    id_cliente
+  ];
+
+  connection.createRow('interesse', values, (err, result) => {
+    if (err) {
+      console.error('Erro ao criar um cliente:', err);
+      res.status(500).json({ error: 'Erro ao criar um cliente' });
+    } else {
+      res.status(200).json({message:'INTERESSE CRIADO COM SUCESSO'})    
     }
   });
 });
@@ -81,10 +217,28 @@ router.delete('/api/delete_client', (req, res) => {
   });
 });
 
+
+
+router.delete('/api/delete_vendedor', (req, res) => {
+  const { id } = req.body;
+  const values = [id];
+  connection.deleteRow('vendedor', id, (err, result) => {
+    if (err) {
+      console.error('Erro ao deletar um vendedores:', err);
+      res.status(500).json({ error: 'Erro ao deletar um vendedores' });
+    } else {
+      console.log('Cliente deletado com sucesso!');
+      res.status(200).json({ message: 'Vendedores deletado com sucesso!' });
+    }
+  });
+});
+
+
+
 router.get('/api/search_clients', (req, res) => {
   const { name } = req.query;
   console.log(name);
-  connection.research_by_name('clientes', name, (err, results) => {
+  connection.research_by_name('cliente', name, (err, results) => {
     if (err) {
       console.error('Erro ao procurar cliente com nome:', { name }, err);
       res.status(500).json({ error: 'Erro ao procurar cliente' });
@@ -96,13 +250,131 @@ router.get('/api/search_clients', (req, res) => {
 });
 
 router.get('/api/search_all_clients', (req, res) => {
-  connection.search_all('clientes', (err, results) => {
+  connection.search_all('cliente', (err, results) => {
     if (err) {
       console.error('Erro ao procurar clientes', err);
       res.status(500).json({ error: 'Erro ao procurar clientes' });
     } else {
       console.log('Busca realizada com sucesso!');
       res.status(200).json(results); // Envie os resultados da pesquisa de volta para o cliente
+    }
+  });
+});
+
+router.get('/api/search_all_vendedores', (req, res) => {
+  connection.search_all('vendedor', (err, results) => {
+    if (err) {
+      console.error('Erro ao procurar clientes', err);
+      res.status(500).json({ error: 'Erro ao procurar clientes' });
+    } else {
+      console.log('Busca realizada com sucesso!');
+      res.status(200).json(results); // Envie os resultados da pesquisa de volta para o cliente
+    }
+  });
+});
+router.get('/api/search_all_abordagens', (req, res) => {
+  connection.search_all('abordagem', (err, results) => {
+    if (err) {
+      console.error('Erro ao procurar clientes', err);
+      res.status(500).json({ error: 'Erro ao procurar clientes' });
+    } else {
+      console.log('Busca realizada com sucesso!');
+      res.status(200).json(results); // Envie os resultados da pesquisa de volta para o cliente
+    }
+  });
+});
+
+router.get("/api/cliente/:clienteId/interesses", (req, res) => {
+  const clienteId = req.params.clienteId;
+
+  // Consulta SQL para obter os interesses do cliente com base no 'clienteId'.
+  connection.searchByIdClient('interesse', [clienteId], (err, results) => {
+    if (err) {
+      console.error("Erro ao buscar interesses:", err);
+      res.status(500).json({ error: "Erro ao buscar interesses" });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+router.get("/api/cliente/:clienteId/etapa", (req, res) => {
+  const clienteId = req.params.clienteId;
+
+  // Consulta SQL para obter os interesses do cliente com base no 'clienteId'.
+  connection.searchByIdClient('abordagem', [clienteId], (err, results) => {
+    if (err) {
+      console.error("Erro ao buscar interesses:", err);
+      res.status(500).json({ error: "Erro ao buscar interesses" });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+// router.get('/api/search_vendedor/:id', (req, res) => {
+//   const vendedorId = req.params.id;
+//   connection.research_by_id('vendedor', vendedorId, (err, results) => {
+//     if (err) {
+//       console.error('Erro ao procurar vendedor', err);
+//       res.status(500).json({ error: 'Erro ao procurar vendedor' });
+//     } else {
+//       if (results.length > 0) {
+//         console.log('Vendedor encontrado!');
+//         res.status(200).json(results[0]); // Envie o vendedor encontrado de volta para o cliente
+//       } else {
+//         console.log('Nenhum vendedor encontrado com o ID fornecido.');
+//         res.status(404).json({ error: 'Nenhum vendedor encontrado com o ID fornecido' });
+//       }
+//     }
+//   });
+// });
+
+router.get('/api/get_etapa_client/:id', (req, res) => {
+  const clienteId = req.params.id; // Alterado para acessar o parâmetro 'id' corretamente.
+
+  // Consulta SQL para obter as informações do cliente com base no 'clienteId'.
+  connection.getEtapaAndClient(clienteId, (err, results) => {
+    if (err) {
+      console.error("Erro ao buscar informações do cliente:", err);
+      res.status(500).json({ error: "Erro ao buscar informações do cliente" });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+router.get('/api/search_by_cpf/:cpf', (req, res) => {
+  const { cpf } = req.params; // Obtenha o CPF dos parâmetros da URL
+
+  if (!cpf) {
+    return res.status(400).json({ error: 'CPF não fornecido' });
+  }
+
+  connection.research_by_cpf('vendedor', cpf, (err, results) => {
+    if (err) {
+      console.error('Erro ao procurar clientes', err);
+      res.status(500).json({ error: 'Erro ao procurar clientes' });
+    } else {
+      console.log('Busca realizada com sucesso!');
+      res.status(200).json(results); // Envie os resultados da pesquisa de volta para o cliente
+    }
+  });
+});
+
+router.post("/api/login", (req, res) => {
+  const { cpf, password } = req.body; // Use req.body para acessar os dados no corpo da solicitação
+  connection.login(cpf, password, (err, results) => {
+    if (err) {
+      console.error("Erro ao fazer login", err);
+      res.status(500).json({ error: "Erro ao procurar cliente" });
+    } else {
+      console.log("Busca realizada com sucesso!");
+      if (results.length === 0) {
+        res.status(401).json({ error: "Credenciais inválidas" });
+      } else {
+        res.status(200).json(results); // Envie os resultados da pesquisa de volta para o cliente
+      }
     }
   });
 });
